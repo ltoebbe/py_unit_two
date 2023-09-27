899dd0d5cc90d75765ec7026d6c4469de1aea1b8
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-09-27 08:58:09 -0400

    :P

---
+++ hours.py
@@ -0,0 +1,5 @@
+hours = input("how many hours did you work?")
+rate = input("what is your rate?")
+pay = hours * rate
+
+print("you are paid", pay)
---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun1 = input("noun:")
+name1 = input("name:")
+pronoun_singular = input("singular pronoun:")
+verb1 = input("verb:")
+plural_noun = input("plural noun:")
+adjective1 = input("adjective:")
+adjective2 = input("adjective:")
+
+print("there once was a", noun1, "named", name1,".", pronoun_singular, "wanted nothing more than to", verb1, "at the grand gathering of the", plural_noun, "but", pronoun_singular, "had a problem.", pronoun_singular, "was too", adjective1, "! the", plural_noun, "thought", pronoun_singular, "was downright", adjective2, ".", name, "decided to infiltrate the gathering with the help of", pronoun_singular_possessive, "trusty", noun2, "and faithful companion", name2, "a", noun3, "with a knack for", ing_verb, ". in the dead of night", name1, "and", name2, past_tense_verb, adverb, "into the ring of", kind_of_house_plural)
